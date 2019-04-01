c0bd3c67fc2bb0523b5e8432a779bcead64c8aa5
Alexandre Ichida <yukioichida@gmail.com>
2019-04-01 16:52:20 -0300

    model

--- model.py
+++ model.py
@@ -1,10 +1,16 @@
 import torch
 import torch.nn as nn
+import numpy as np
 
 class ModelAttention(nn.Module):
     ''' Attention Model '''
 
     def __init__(self, dim_model)
+        # root square of dimension size
+        self.temper = np.power(dim_model, 0.5)
+        
+
+    def forward(self, q, k, v):
 
 
 class MultiHeadAttention(nn.Module):
@@ -21,4 +27,6 @@ class MultiHeadAttention(nn.Module):
         self.weight_k = nn.Parameter(torch.FloatTensor(n_head, dim_model, dim_k))
         self.weight_v = nn.Parameter(torch.FloatTensor(n_head, dim_model, dim_v))
         
+
+
 

9bd32cb36c5f767f0e97bf53b833f7c5d8eb1aae
Alexandre Ichida <yukioichida@gmail.com>
2019-05-19 01:23:57 -0300

    include test validation

--- train.py
+++ train.py
@@ -132,3 +132,42 @@ trainer.add_event_handler(Events.EPOCH_COMPLETED, log_validation_results)
 
 # TODO: model checkpoint
 trainer.run(train_iter, max_epochs=MAX_EPOCH)
+
+
+def binary_accuracy(preds, y):
+    """
+    Returns accuracy per batch, i.e. if you get 8/10 right, this returns 0.8, NOT 8
+    """
+
+    # round predictions to the closest integer
+    rounded_preds = torch.round(torch.sigmoid(preds))
+    correct = (rounded_preds == y).float()  # convert into float for division
+    acc = correct.sum() / len(correct)
+    return acc
+
+
+def evaluate(iterator):
+    epoch_loss = 0
+    epoch_acc = 0
+
+    model.eval()
+
+    with torch.no_grad():
+        for batch in iterator:
+            text, text_lengths = batch.text
+
+            predictions = model(text).squeeze(1)
+
+            loss = loss_function(predictions, batch.label)
+
+            acc = binary_accuracy(predictions, batch.label)
+
+            epoch_loss += loss.item()
+            epoch_acc += acc.item()
+
+    return epoch_loss / len(iterator), epoch_acc / len(iterator)
+
+
+test_loss, test_acc = evaluate(test_iter)
+
+print(f'Test Loss: {test_loss:.3f} | Test Acc: {test_acc * 100:.2f}%')
